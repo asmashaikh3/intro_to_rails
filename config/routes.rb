1b5ae6dc84c2065081fb9d268b11dff1a7d194d7
asmashaikh3 <ashaikh3@academic.rrc.ca>
2024-07-04 15:10:33 -0500

    menu feature implemented

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get 'about/index'
   get 'about', to: 'static_pages#about'
   resources :books do
-    resources :ratings, only: [:index, :new, :create]
-    resources :reviews, only: [:index, :new, :create]
+    resources :ratings, only: [:index, :new, :create, :destroy]
+    resources :reviews, only: [:index, :new, :create, :destroy]
     resources :books, only: [:index, :show]
   end
   resources :users, only: [:index, :show, :new, :create]
